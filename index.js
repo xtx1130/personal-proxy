d1cdaad5ef3c8c5b034cd42430c4bb2398ade928
xtx1130 <xtx1130@gmail.com>
2017-08-07 16:11:12 +0800

    add http socket support

--- index.js
+++ index.js
@@ -55,9 +55,55 @@ router.post('*', async ctx => {
 });
 app.use(router.routes());
 
-http.createServer(app.callback()).listen(8888,() => {
+//socket proxy
+let socketProxy = (req,socket,head) => {
+	let uri = {
+		host: req.url.split(':')[0],
+		port: req.url.split(':')[1] || 5390
+	},
+	headers = {
+        'Connection': 'keep-alive',
+        'Proxy-Agent': 'Qiyi Proxy'
+	},
+	socketConnect = net.createConnection(uri,() => {
+		let cb = err => {
+			if(err){
+				console.log('request error:'+err.message);
+				socketConnect.end();
+				socket.end();
+				throw new Error(err);
+			}else{
+				socketConnect.pipe(socket);
+                socket.pipe(socketConnect);
+			}
+		};
+		try{
+			let status = 'HTTP/1.1 200 Connection established\r\n',
+				headerLines = '';
+			for(let key in headers)
+				headerLines += key + ': ' + headers[key] + '\r\n';
+			 socket.write(status + headerLines + '\r\n', 'UTF-8', cb);
+		}catch(e){
+			console.log('socketConnect error:',+ e.message);
+		}
+	});
+	socketConnect.setNoDelay(true);
+	socketConnect.on('error', e => {
+		console.log(e)
+	});
+}
+//http proxy
+let httpServer = http.createServer(app.callback());
+httpServer.on('connect',socketProxy);
+httpServer.listen(8888,() => {
   	console.log('http server is running at 8888');
 });
-https.createServer(options, app.callback()).listen(8089, function () {
+//https proxy
+let httpsServer = https.createServer(options, app.callback());
+httpsServer.on('connect',function(req,res){
+	//TO DO :整合http和https加入socket支持
+	console.log('httpsServer')
+});
+httpsServer.listen(8089, function () {
     console.log('Https server listening on port 8089');
 });
