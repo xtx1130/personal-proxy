317fc763cbf89d171aa59b1e737cc778ac3f446f
xtx1130 <xtx1130@gmail.com>
2017-10-10 18:06:30 +0800

    change ip to hostname

--- plugins/httpProxy.js
+++ plugins/httpProxy.js
@@ -7,11 +7,11 @@ const router = new Router();
 const proxyJs = new httpProxy.createProxyServer({});
 
 router.get('*', async ctx => {
-    proxyJs.web(ctx.req, ctx.res,{target:'http://127.0.0.1:6666',toProxy:true,prependPath:false});
+    proxyJs.web(ctx.req, ctx.res,{target:'http://proxy.iqiyi.com',toProxy:true,prependPath:false});
     ctx.body = ctx.res;
 });
 router.post('*', async ctx => {
-    proxyJs.web(ctx.req, ctx.res,{target:'http://127.0.0.1:6666',toProxy:true,prependPath:false});
+    proxyJs.web(ctx.req, ctx.res,{target:'http://proxy.iqiyi.com',toProxy:true,prependPath:false});
     ctx.body = ctx.res;
 });
 
