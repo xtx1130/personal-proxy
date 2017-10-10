4f7bf368b2c156d05235f90da3624fe3e4b61834
xtx1130 <xtx1130@gmail.com>
2017-10-10 10:25:21 +0800

    change port & change shell

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:latest
 RUN mkdir -p /usr/src/personal-proxy
 WORKDIR /usr/src/personal-proxy
 COPY . /usr/src/personal-proxy
-RUN cd /usr/src/personal-proxy;npm install
+RUN cd /usr/src/personal-proxy;npm install;npm install -g forever
+CMD ["/usr/src/personal-proxy/start.sh","2"]
 EXPOSE 8080
 EXPOSE 8081
-RUN /usr/src/personal-proxy/start.sh 2
--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ cd /usr/src/personal-proxy
 num=$1;
 for((i=0;i<$num;i++)){
       port=$(($i+8080));
-      forever start -a -l /usr/src/personal-proxy/Log/forever/logs.log -o /usr/src/personal-proxy/Log/forever/o.log -e /usr/src/personal-proxy/Log/forever/error.log /usr/src/personal-proxy/index.js $port t
+      forever start -a -c 'node --harmony' /usr/src/personal-proxy/index.js $port
 }
-exit 1;
+tail -f /dev/null;
+exit 0;
